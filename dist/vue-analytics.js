fed1c9ef19868706b36a31867c78b28327a1edbc
Matteo Gabriele <matteo@blocklevel.nl>
2017-01-02 11:16:34 +0100

    :package:

--- dist/vue-analytics.js
+++ dist/vue-analytics.js
@@ -1,154 +1,160 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else {
-		var a = factory();
-		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
-	}
-})(this, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
+/*!
+ * vue-analytics v1.2.0
+ * (c) 2017 Matteo Gabriele
+ * Released under the ISC License.
+ */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.VueAnalytics = global.VueAnalytics || {})));
+}(this, (function (exports) { 'use strict';
 
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
+/**
+ * Configuration
+ * @type {Object}
+ */
+var config = {
+  debug: false,
+  excludes: []
+};
 
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
+/**
+ * Event tracking
+ * @param  {String} category
+ * @param  {String} action
+ * @param  {String} [label='']
+ * @param  {Number} [value=0]
+ */
+var event = function event(category, action) {
+  var label = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
+  var value = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
 
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
+  if (typeof window.ga === 'undefined') {
+    return;
+  }
 
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+  if (config.debug) {
+    /* eslint-disable */
+    console.groupCollapsed('[VueAnalytics] Track event category "' + category + '"');
+    console.log('category: ' + category);
+    console.log('action: ' + action);
+    console.log('label: ' + label);
+    console.log('value: ' + value);
+    console.groupEnd();
+    /* eslint-enable */
+  }
 
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+  window.ga('send', 'event', category, action, label, value);
+};
 
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+/**
+ * Page tracking
+ * @param  {String} page
+ * @param  {String} title
+ * @param  {String} location
+ */
+var page = function page(_page) {
+  var title = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+  var location = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
 
+  if (typeof window.ga === 'undefined') {
+    return;
+  }
 
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
+  if (config.debug) {
+    /* eslint-disable */
+    console.groupCollapsed('[VueAnalytics] Track page "' + _page + '"');
+    console.log('page: ' + _page);
+    console.log('title: ' + title);
+    console.log('location: ' + location);
+    console.groupEnd();
+    /* eslint-enable */
+  }
 
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
+  window.ga('send', 'pageview', { page: _page, title: title, location: location });
+};
 
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/**
+ * Google Analytics script loader
+ * it auto adds Google Analytics script without needs to modify the HTML page.
+ * @param  {String} id Google Analytics ID
+ * @return {Promise}
+ */
+var loadScript = function loadScript(id) {
+  return new Promise(function (resolve, reject) {
+    var script = document.createElement('script');
+    var prior = document.getElementsByTagName('script')[0];
 
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+    script.async = 1;
 
-	module.exports = __webpack_require__(1);
+    prior.parentNode.insertBefore(script, prior);
 
+    script.onload = script.onreadystatechange = function (_, isAbort) {
+      if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+        script.onload = script.onreadystatechange = null;
+        script = undefined;
 
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
+        if (isAbort) {
+          reject({ error: true });
+          return;
+        }
 
-	'use strict';
+        window.ga('create', id, 'auto');
 
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
+        resolve({ success: true, id: id });
+      }
+    };
+    script.src = '//www.google-analytics.com/analytics.js';
+  });
+};
 
-	var config = {
-	  debug: false,
-	  excludes: []
-	};
-
-	var event = function event(category, action) {
-	  var label = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
-	  var value = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
-
-	  if (typeof window.ga === 'undefined') {
-	    return;
-	  }
-
-	  if (config.debug) {
-	    console.groupCollapsed('[VueAnalytics] Track event category "' + category + '"');
-	    console.log('category: ' + category);
-	    console.log('action: ' + action);
-	    console.log('label: ' + label);
-	    console.log('value: ' + value);
-	    console.groupEnd();
-	  }
-
-	  window.ga('send', 'event', category, action, label, value);
-	};
-
-	var page = function page(_page) {
-	  var title = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
-	  var location = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
-
-	  if (typeof window.ga === 'undefined') {
-	    return;
-	  }
-
-	  if (config.debug) {
-	    console.groupCollapsed('[VueAnalytics] Track page "' + _page + '"');
-	    console.log('page: ' + _page);
-	    console.log('title: ' + title);
-	    console.log('location: ' + location);
-	    console.groupEnd();
-	  }
-
-	  window.ga('send', 'pageview', { page: _page, title: title, location: location });
-	};
-
-	var install = function install(Vue) {
-	  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-	  var router = options.router,
-	      debug = options.debug,
-	      excludeRoutes = options.excludeRoutes;
-
-
-	  config.excludes = excludeRoutes || config.excludes;
-	  config.debug = !!debug;
-
-	  Vue.track = Vue.ga = { event: event, page: page };
-
-	  Vue.$track = { event: event, page: page };
-
-	  Vue.prototype.$track = Vue.prototype.$ga = { event: event, page: page };
-
-	  if (router) {
-	    (function () {
-	      var excludes = config.excludes;
+/**
+ * Vue installer
+ * @param  {Vue instance} Vue
+ * @param  {Object} [options={}]
+ */
+var install = function install(Vue) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var router = options.router,
+      debug = options.debug,
+      excludeRoutes = options.excludeRoutes;
 
 
-	      router.afterEach(function (_ref) {
-	        var path = _ref.path,
-	            name = _ref.name;
+  config.excludes = excludeRoutes || config.excludes;
+  config.debug = !!debug;
 
-	        if (excludes.length && excludes.indexOf(name) !== -1) {
-	          return;
-	        }
+  /**
+   * Naming conventions
+   * Using "track" for semantic purposes
+   * Using "ga", as an alias, for a more familiar feeling
+   */
+  Vue.track = Vue.ga = { event: event, page: page };
+  Vue.prototype.$track = Vue.prototype.$ga = { event: event, page: page };
 
-	        Vue.track.page(path, name, window.location.href);
-	      });
-	    })();
-	  }
-	};
+  if (router) {
+    (function () {
+      var excludes = config.excludes;
 
-	exports.default = { install: install };
 
-/***/ }
-/******/ ])
-});
-;
+      router.afterEach(function (_ref) {
+        var path = _ref.path,
+            name = _ref.name;
+
+        if (excludes.length && excludes.indexOf(name) !== -1) {
+          return;
+        }
+
+        Vue.track.page(path, name, window.location.href);
+      });
+    })();
+  }
+};
+
+var index = { install: install };
+
+exports.loadScript = loadScript;
+exports['default'] = index;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
