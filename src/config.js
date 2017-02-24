9facf49e05e6b6aba48d588225157756ee728f40
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-24 19:39:13 +0100

    fix(config): add recursive merge
    closes #13

--- src/config.js
+++ src/config.js
@@ -24,6 +24,11 @@ let config = {
  */
 const merge = function (obj, src) {
   Object.keys(src).forEach(function (key) {
+    if (obj[key] && typeof obj[key] === 'object') {
+      merge(obj[key], src[key])
+      return
+    }
+
     obj[key] = src[key]
   })
 
