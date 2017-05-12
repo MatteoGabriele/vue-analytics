c537a6834a1fd3a4720ba2421421f70a04b4dda0
Matteo Gabriele <matteo@blocklevel.nl>
2017-05-12 23:14:21 +0200

    fix(auto-tracking): pageviewTemplate only on page load
    closes #38

--- src/features/page.auto.js
+++ src/features/page.auto.js
@@ -3,6 +3,10 @@ import { exists } from '../utils'
 import page from './page'
 import set from './set'
 
+function template (proxy, router) {
+  return proxy ? proxy(router.currentRoute) : router
+}
+
 /**
  * Enable route autoTracking page
  * @param  {VueRouter} router
@@ -14,20 +18,19 @@ export default function autoTrackPage (router) {
 
   const { currentRoute } = router
   const pageviewProxyFn = config.autoTracking.pageviewTemplate
-  const pageviewTemplate = pageviewProxyFn ? pageviewProxyFn(currentRoute) : router
 
   if (!exists(currentRoute.name) && config.autoTracking.pageviewOnLoad) {
     set('page', currentRoute.path)
-    page(pageviewTemplate)
+    page(template(pageviewProxyFn, router))
   }
 
   // Track all other pages
-  router.afterEach(function ({ path, name }) {
+  router.afterEach(function ({ name, path }) {
     if (exists(name)) {
       return
     }
 
     set('page', path)
-    page(pageviewTemplate)
+    page(template(pageviewProxyFn, router))
   })
 }
