8c45f02e2abdda85eec262d93165641a1f78731f
Matteo Gabriele <matteo@blocklevel.nl>
2017-08-19 12:47:12 +0200

    refactor(set): refactor set method

--- src/lib/set.js
+++ src/lib/set.js
@@ -1,3 +1,17 @@
-export default function set () {
-  console.log('set!')
+import query from 'lib/query'
+
+export default function set (...args) {
+  if (typeof args[0] === 'object' && args[0].constructor === Object) {
+    query('set', args[0])
+    return
+  }
+
+  if (args.length < 2 || (typeof args[0] !== 'string' && typeof args[1] !== 'string')) {
+    throw new Error(
+      '[vue-analytics] $ga.set needs a field name and a field value, or you can pass an object literal'
+    )
+  }
+
+  // field name and field value
+  query('set', args[0], args[1])
 }
