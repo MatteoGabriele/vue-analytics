4b1c85df4605125b76241cd2855eaa1f09ea49ae
Matteo Gabriele <matteo@blocklevel.nl>
2017-09-07 10:27:27 +0200

    test(helpers): add getQueryString test

--- __tests__/helpers.spec.js
+++ __tests__/helpers.spec.js
@@ -2,14 +2,14 @@ import * as helpers from '../src/helpers'
 
 const id = 'UA-1234-5'
 
-describe('noop', function () {
-  it ('should be a function', function () {
+describe('noop', () => {
+  it ('should be a function', () => {
     expect(typeof helpers.noop).toEqual('function')
   })
 })
 
-describe('merge', function () {
-  it ('should merge two objects', function () {
+describe('merge', () => {
+  it ('should merge two objects', () => {
     const a = { a: 1, c: { a: 1 } }
     const b = { b: 1, c: { b: 1 } }
     const c = helpers.merge(a, b)
@@ -25,9 +25,25 @@ describe('merge', function () {
   })
 })
 
-describe('getTracker', function () {
-  it ('should return the tracking id without dashes', function () {
+describe('getTracker', () => {
+  it ('should return the tracking id without dashes', () => {
     const tracker1 = helpers.getTracker(id)
     expect(tracker1).toEqual('UA12345')
+  })
+})
+
+describe('getQueryString', () => {
+  it ('should return a query string from an object literal', () => {
+    const obj = { name: 'matteo', surname: 'gabriele' }
+    const queryString = helpers.getQueryString(obj)
+
+    expect(queryString).toEqual('?name=matteo&surname=gabriele')
+  })
+
+  it ('should return a empty string if object literal is empty', () => {
+    const obj = {}
+    const queryString = helpers.getQueryString(obj)
+
+    expect(queryString).toEqual('')
   })
 })
