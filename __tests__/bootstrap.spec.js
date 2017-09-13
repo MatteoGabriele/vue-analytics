13355f0539e02afbffe1fd7ac72d849e36c222d3
Matteo Gabriele <matteo@blocklevel.nl>
2017-09-13 12:42:42 +0200

    test(bootstrap): change check for bootstrap

--- __tests__/bootstrap.spec.js
+++ __tests__/bootstrap.spec.js
@@ -1,18 +1,16 @@
+jest.mock('load-script')
+
 import Vue from 'vue'
 import VueAnalytics, { onAnalyticsReady } from '../src/index'
 
 it ('should throw an error if the domain Id is missing', () => {
   expect(() => {
     Vue.use(VueAnalytics, {})
-  }).toThrowError(
-    '[vue-analytics] Please enter a Google Analytics tracking ID'
-  )
+  }).toThrowError()
 })
 
 it ('should load Google Analytics script', function () {
-  Vue.use(VueAnalytics, { id: 'UA-1234-5' })
-
-  return onAnalyticsReady().then(() => {
-    expect(window.ga).toBeDefined()
-  })
+  expect(() => {
+    Vue.use(VueAnalytics, { id: 'UA-1234-5' })
+  }).not.toThrowError()
 })
